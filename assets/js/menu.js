eaf4e383ae2e28db0d1c8a653626a4b96cf3ac78
Felipe <fee.rocha@hotmail.com>
2020-05-29 00:21:03 -0300

    Ajsutes para funcionar no safari

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,8 +1,12 @@
-const menuButtons = document.querySelectorAll('.menu-button');
-menuButtons.forEach(btn => btn.addEventListener('click', selectContent));
+const events = ['click', 'touchstart']
 
+const menuButtons = document.querySelectorAll('.menu-button');
 const backButtons = document.querySelectorAll('.title > img');
-backButtons.forEach(btn => btn.addEventListener('click', backHome));
+
+events.forEach(event => {
+  menuButtons.forEach(btn => btn.addEventListener(event, selectContent));
+  backButtons.forEach(btn => btn.addEventListener(event, backHome));
+})
 
 let contentSection;
 
